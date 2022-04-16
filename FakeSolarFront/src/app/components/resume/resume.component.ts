4d1e769b74ab4f7309df62388af4cdd90aed78d3
MarcRodriguez <marc161297@gmail.com>
2022-04-16 14:27:01 +0100

    Add resume component

--- FakeSolarFront/src/app/components/resume/resume.component.ts
+++ FakeSolarFront/src/app/components/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  warnings: number = 0;
+  criticals: number = 0;
+  inspsWarnings: number = 0;
+  inspsCriticals: number = 0;
+
+
+  constructor( private dataService: DataService) { }
 
   ngOnInit(): void {
+
+    this.dataService.getInspections()
+      .subscribe( (inspections: any) => {
+        for (let inspection of inspections) {
+          this.warnings += inspection.issuesWarningCount
+          this.criticals += inspection.issuesCriticalCount
+          if (inspection.issuesCriticalCount > 0) {
+            this.inspsCriticals += 1
+          } else if (inspection.issuesWarningCount > 0) {
+            this.inspsWarnings += 1
+          }
+        }
+      })
+      
   }
 
 }
