82e29c6a7246f4d3505e81071a0fba41d9d8400e
MarcRodriguez <marc161297@gmail.com>
2022-04-16 11:05:08 +0100

    Create Inspection Model

--- FakeSolar/API/models.py
+++ FakeSolar/API/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Inspection(models.Model):
+    title = models.CharField(max_length=256)
+    inspectorName = models.CharField(max_length=256)
+    itemsOk = models.IntegerField()
+    issuesWarningCount = models.IntegerField(blank=True, default=0)
+    issuesCriticalCount = models.IntegerField(blank=True, default=0)
+    company = models.CharField(max_length=256)
